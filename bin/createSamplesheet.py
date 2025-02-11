52f9c3bf466006cb000142761ff644c672479d9a
Manlove <manlove.logan@gmail.com>
2025-02-10 22:19:23 -0800

    resolved issue with single end read not being added correctly, added sample name to classes

--- bin/createSamplesheet.py
+++ bin/createSamplesheet.py
@@ -5,10 +5,11 @@ import re
 fastq_path = "/Users/logan/Documents/SampleSheetGenerator/FASTQ/"
 
 class SingleRead():
-    def __init__(self, file_path, basename):
+    def __init__(self, file_path, sample_name, basename):
+        self.sample_name = sample_name
         self.file_path = file_path
         self.files = ["",""]
-        self.AddFile(basename)
+        self.AddFile(file_path, basename)
 
     def AddFile(self,file_path,basename):
         if file_path == self.file_path:
@@ -20,11 +21,11 @@ class SingleRead():
             print("Bad Path: {}".format(file_path))
 
     def GetReads(self):
-        print(self.files)
-
+        return self.files
 
 class ReadPair(SingleRead):
-    def __init__(self, file_path, basename, read_match):
+    def __init__(self, file_path, sample_name, basename, read_match):
+        self.sample_name = sample_name
         self.read_match = read_match[:-2]
         self.file_path = file_path
         self.files = ["",""]
@@ -49,7 +50,7 @@ fastq_files = {}
 for i in glob.glob(os.path.join(fastq_path,"lung*")):
     fastq_file = list(os.path.splitext(i))
     fastq_directory = os.path.dirname(i)
-
+    
     # Checks if the file is zipped and expands the extension to add the file type.
     if fastq_file[1] == ".gz":
         temp_file = list(os.path.splitext(fastq_file[0]))
@@ -58,31 +59,32 @@ for i in glob.glob(os.path.join(fastq_path,"lung*")):
 
     # Gets the basename of the file
     basename = os.path.basename(i)
-    print(basename)
     
     # Finds the read number if one exists
     pattern = re.search(r"_[rR]?[12]\.",basename)
+    
+    sample_name = re.sub(r"_?[rR]?[12]?\..*","",basename)
+
     if pattern:
 
         # Removes the read number (ex. _R1) from the sample name
-        sample_name = basename.replace(pattern[0], pattern[0][:-2]+".")
+        stripped_name = basename.replace(pattern[0], pattern[0][:-2]+".")
 
-        if sample_name in fastq_files:
-            fastq_files[sample_name].AddFile(fastq_directory,basename,pattern[0])
+        if stripped_name in fastq_files:
+            fastq_files[stripped_name].AddFile(fastq_directory,basename,pattern[0])
         else:
-            fastq_files[sample_name] = ReadPair(fastq_directory,basename,pattern[0])
+            fastq_files[stripped_name] = ReadPair(fastq_directory,sample_name,basename,pattern[0])
 
     else:
-        if sample_name in fastq_files:
-            fastq_files[sample_name].AddFile(fastq_directory,basename)
+
+        if basename in fastq_files:
+            fastq_files[basename].AddFile(fastq_directory,basename)
         else:
-            fastq_files[sample_name] = SingleRead(fastq_directory,basename)
-        
+            fastq_files[basename] = SingleRead(fastq_directory,sample_name,basename)
+  
 for i in fastq_files:
-    print(i)
+
     print(fastq_files[i].GetReads())
-
-
 
 def CheckExtension(fastqFile):
     pass
