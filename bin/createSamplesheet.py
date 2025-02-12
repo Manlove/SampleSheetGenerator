b6f1ef1f9e4b10a9d2ce0fe59b86364cfea7ac4d
Manlove <manlove.logan@gmail.com>
2025-02-12 15:19:56 -0800

    Some refacturing, added variable type hints

--- bin/createSamplesheet.py
+++ bin/createSamplesheet.py
@@ -1,15 +1,15 @@
 import os
 import glob
 import re
+from typing import List, Dict, Union
 
-class SingleRead():
-    def __init__(self, file_path, sample_name, basename):
+class SingleRead:
+    def __init__(self, file_path: str, sample_name: str) -> None:
         self.sample_name = sample_name
         self.file_path = file_path
-        self.files = ["",""]
-        self.AddFile(file_path, basename)
+        self.files: List[str] = ["",""]
 
-    def AddFile(self,file_path,basename):
+    def add_file(self, file_path: str, basename: str) -> None:
         if file_path == self.file_path:
             if self.files[0] == "":
                 self.files[0] = basename
@@ -18,21 +18,18 @@ class SingleRead():
         else:
             print("Bad Path: {}".format(file_path))
 
-    def GetReads(self):
+    def get_reads(self) -> List[str]:
         return self.files
     
-    def GetName(self):
+    def get_name(self) -> str:
         return self.sample_name
 
 class ReadPair(SingleRead):
-    def __init__(self, file_path, sample_name, basename, read_match):
-        self.sample_name = sample_name
+    def __init__(self, file_path: str, sample_name: str, read_match: str) -> None:
+        super().__init__(file_path, sample_name) 
         self.read_match = read_match[:-2]
-        self.file_path = file_path
-        self.files = ["",""]
-        self.AddFile(file_path,basename,read_match)
 
-    def AddFile(self, file_path, basename, read_match):
+    def add_file(self, file_path: str, basename: str, read_match: str) -> None:
         if file_path == self.file_path:
             if re.match(read_match[:-2], self.read_match):
                 read_number = int(read_match[-2:-1]) - 1
@@ -45,10 +42,10 @@ class ReadPair(SingleRead):
         else:
             print("Bad Path: {}".format(file_path))
 
-def CreateSampleSheet(fastq_files, fastq_path):
+def create_sample_sheet(fastq_files: Dict[str, Union[SingleRead, ReadPair]], fastq_path: str) -> None:
 
     # get a list of all files in the path with the given pattern 
-    for i in glob.glob(os.path.join(fastq_path,"*")):
+    for i in glob.glob(os.path.join(fastq_path, "*")):
         fastq_file = list(os.path.splitext(i))
         fastq_directory = os.path.dirname(i)
         
@@ -56,43 +53,46 @@ def CreateSampleSheet(fastq_files, fastq_path):
         if fastq_file[1] == ".gz":
             temp_file = list(os.path.splitext(fastq_file[0]))
             temp_file[1] += ".gz"
-            fastq_file = temp_file
+            fastq_file  = temp_file
 
         # Gets the basename of the file
         basename = os.path.basename(i)
         
         # Finds the read number if one exists
-        pattern = re.search(r"_[rR]?[12]\.",basename)
+        pattern = re.search(r"_[rR]?[12]\.", basename)
         
-        sample_name = re.sub(r"_?[rR]?[12]?\..*","",basename)
+        sample_name = re.sub(r"_?[rR]?[12]?\..*", "", basename)
 
         if pattern:
 
             # Removes the read number (ex. _R1) from the sample name
-            stripped_name = basename.replace(pattern[0], pattern[0][:-2]+".")
+            stripped_name = basename.replace(pattern[0], pattern[0][:-2] + ".")
 
-            if stripped_name in fastq_files:
-                fastq_files[stripped_name].AddFile(fastq_directory,basename,pattern[0])
-            else:
-                fastq_files[stripped_name] = ReadPair(fastq_directory,sample_name,basename,pattern[0])
+            if stripped_name not in fastq_files:
+                fastq_files[stripped_name] = ReadPair(fastq_directory, sample_name, pattern[0])
+
+            fastq_files[stripped_name].add_file(fastq_directory, basename, pattern[0])
 
         else:
 
-            if basename in fastq_files:
-                fastq_files[basename].AddFile(fastq_directory,basename)
-            else:
-                fastq_files[basename] = SingleRead(fastq_directory,sample_name,basename)
-
-def WriteSampleSheet(fastq_files):
+            if basename not in fastq_files:
+                fastq_files[basename] = SingleRead(fastq_directory, sample_name)
+            
+            fastq_files[basename].add_file(fastq_directory, basename)
+                
+def write_sample_sheet(fastq_files: Dict[str, Union[SingleRead,ReadPair]]) -> None:
     with open("samplesheet.csv","w") as out_file:
         out_file.write("sample,fastq1,fastq2")
         for sample in fastq_files:
-            reads = fastq_files[sample].GetReads()
-            sample_name = fastq_files[sample].GetName()
-            out_file.write("\n{},{},{}".format(sample_name,reads[0],reads[1]))
+            reads = fastq_files[sample].get_reads()
+            sample_name = fastq_files[sample].get_name()
+            out_file.write("\n{},{},{}".format(sample_name, reads[0], reads[1]))
+
+def main(fastq_path: str) -> None:
+    fastq_files: Dict[str, Union[SingleRead, ReadPair]] = {}
+    create_sample_sheet(fastq_files, fastq_path)
+    write_sample_sheet(fastq_files)
 
 if __name__ == "__main__":
-    fastq_path = "/Users/logan/Documents/SampleSheetGenerator/FASTQ/"
-    fastq_files = {}
-    CreateSampleSheet(fastq_files, fastq_path)
-    WriteSampleSheet(fastq_files)
+    fastq_path = "/Users/logan/Documents/SampleSheetGenerator/FASTQ/"   
+    main(fastq_path)
